1cc9c88a3a84e483f7a92146f9415d3a7e6802de
Jocelyn Badgley (Twipped) <joc@twipped.com>
2021-03-02 16:20:35 -0800

    Fixing broken cloudwatch log permissions

--- terraform/tracking.tf
+++ terraform/tracking.tf
@@ -30,8 +30,24 @@ resource "aws_s3_bucket_object" "ipixel" {
   content_type = "image/gif"
 }
 
+data "aws_canonical_user_id" "current" {}
+
 resource "aws_s3_bucket" "ipixel_logs" {
   bucket = "${var.site}-analytics"
+
+  grant {
+    id          = data.aws_canonical_user_id.current.id
+    permissions = ["FULL_CONTROL"]
+    type        = "CanonicalUser"
+  }
+
+  grant {
+    # Grant CloudFront awslogsdelivery logs access to your Amazon S3 Bucket
+    # https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/AccessLogs.html#AccessLogsBucketAndFileOwnership
+    id          = "c4c1ede66af53448b93c283ce9448c4ba468c9432aa01d700d3878632f77d2d0"
+    permissions = ["FULL_CONTROL"]
+    type        = "CanonicalUser"
+  }
 
   lifecycle_rule {
     id      = "logfiles"
