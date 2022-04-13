bfdfd6918d982c480df9f07dc2e073a034f5fb9a
G. Queiroz <gabrieljvnq@gmail.com>
2022-04-13 02:07:44 -0300

    WARC generation almost done

--- gulp/offline2.js
+++ gulp/offline2.js
@@ -14,11 +14,13 @@ class GDBWarc {
 	#hostMap;
 	#domainsToInclude;
 	#pagesToInclude;
+	#downloadedFiles;
 	#pages;
 
 	static user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36";
 	static main_domain = 'genderdysphoria.fyi';
 	static main_base_url = 'https://'+GDBWarc.main_domain;
+	static urlCssRegexp = /url\((https?:\/\/[^)]+)\)/ig;
 
 	constructor(filename, proxy_url) {
 		this.#filename = filename;
@@ -34,10 +36,12 @@ class GDBWarc {
 		this.#domainsToInclude = {
 			'fonts.googleapis.com': true,
 			'fonts.gstatic.com': true,
-			'cdnjs.cloudflare.com': true
+			'cdnjs.cloudflare.com': true,
+			'twemoji.maxcdn.com': true
 		};
 		this.#domainsToInclude[GDBWarc.main_domain] = true;
-		this.#pagesToInclude = ['/', '/pt', /^\/pt\/.*/i];
+		this.#pagesToInclude = ['/', /^\/pt(\/.*)?/i, /^\/en(\/.*)?/i, /^\/zh(\/.*)?/i];
+		this.#downloadedFiles = {};
 		this.#pages = [];
 
 		this.#status = 0;
@@ -52,14 +56,22 @@ class GDBWarc {
 		this.#writer.writeWarcInfoRecord({});
 	}
 
-	async addPage(url, recursive) {
+	parseURL(url) {
+		return new URL(url, GDBWarc.main_base_url);
+	}
+
+	async addPage(url, page_recursion, dep_recursion) {
 		if (this.#status < 1) {
 			this.start();
 		}
-		await this.#add_page(new URL(url, GDBWarc.main_base_url), recursive);
+		await this.#add_page(this.parseURL(url), page_recursion, dep_recursion);
 	}
 
 	should_add_page(url) {
+		if (!(url instanceof URL)) {
+			throw new Error('Only URL objects allowed');
+		}
+
 		if (url.host !== GDBWarc.main_domain) {
 			return false;
 		}
@@ -79,10 +91,18 @@ class GDBWarc {
 	}
 
 	should_add_file(url) {
-		return this.#domainsToInclude[url.host];		
+		if (!(url instanceof URL)) {
+			throw new Error('Only URL objects allowed');
+		}
+
+		return this.#domainsToInclude[url.host];
 	}
 
 	get_real_url(url) {
+		if (!(url instanceof URL)) {
+			url = this.parseURL(url);
+		}
+
 		const real = this.#hostMap[url.host];
 		if (real !== undefined) {
 			const ans = new URL(url.toString());
@@ -98,10 +118,25 @@ class GDBWarc {
 	}
 
 	get_http(url) {
+		if (!(url instanceof URL)) {
+			throw new Error('Only URL objects allowed');
+		}
+
+		const real_url = this.get_real_url(url);
+		if (real_url.protocol === 'https:') {
+			real_url.protocol = 'http:';
+		}
+		
 		return new Promise((resolve, reject) => {
-			const real_url = this.get_real_url(url);
-			var req = http.get(real_url.toString(), (res) => {
-				var response_headers = '';
+			var req = http.get(
+				real_url.toString(),
+				{
+					headers: {
+						'User-Agent': GDBWarc.user_agent
+					}
+				},
+			 	(res) => {
+				var response_headers = `HTTP/${res.httpVersion} ${res.statusCode} ${res.statusMessage}\r\n`;
 				for (var i=0; i < res.rawHeaders.length; i++) {
 					if (i%2 == 0) {
 						response_headers += res.rawHeaders[i]+': ';
@@ -114,14 +149,28 @@ class GDBWarc {
 					`Host: ${real_url.host}\r\n`,
 					`Host: ${url.host}\r\n`).replace('\r\n\r\n', '\r\n');
 
-				let rawData = '';
-				res.on('data', (chunk) => { rawData += chunk; });
+				const full_content_type = (res.headers['content-type']||'');
+				const content_type = full_content_type.split(';')[0];
+
+				res.setEncoding('binary');
+				let chunks = [];
+				res.on('data', (chunk) => {
+					chunks.push(Buffer.from(chunk, 'binary'));
+				});
 				res.on('end', (chunk) => {
+					const data = Buffer.concat(chunks);
+					var body = null;
+					if (full_content_type.includes('charset=UTF-8') || full_content_type.includes('charset=utf-8')) {
+						body = new TextDecoder().decode(data);
+					}
 					resolve({
+						'status_code': res.statusCode,
+						'content_type': content_type,
 						'response_headers': res.headers,
 						'response_headers_raw': response_headers,
 						'request_headers_raw': request_headers,
-						'data': rawData
+						'data': data,
+						'body': body
 					})
 				});
 			});
@@ -133,16 +182,93 @@ class GDBWarc {
 		});
 	}
 
-	async #add_page(url, recursive) {
-		const dependencies = [];
-		const page_links = [];
+	async #add_page(url, page_recursion, dep_recursion) {
+		url.hash = '';
+		if (page_recursion === 0 || dep_recursion === 0 || this.#downloadedFiles[url.toString()]) {
+			return;
+		}
+		this.#downloadedFiles[url.toString()] = true;
 
+		const dependencies = {};
+		const page_links = {};
+
+		console.log("Downloading: "+url.toString());
 		const req = await this.get(url);
 
+		// Write data
 		await this.#writer.writeRequestRecord(url, req.request_headers_raw);
-		await this.#writer.writeResponseRecord(url, req.response_headers_raw, req.data);
+		if (req.body) {
+			await this.#writer.writeResponseRecord(url, req.response_headers_raw, req.body);
+		} else {
+			await this.#writer.writeResponseRecord(url, req.response_headers_raw, req.data);
+		}
+		if (req.content_type === 'text/html' && req.status_code === 200) {
+			this.#pages.push(url.toString());
+		}
 
-		this.#pages.push(url.toString());
+		// Find dependencies
+		if (req.content_type === 'text/html') {
+			// Parse and look for links
+			const root = xpathhtml.fromPageSource(req.body);
+			
+			const links = root.findElements("//*[@href]");
+			for (const link of links) {
+				// Don't download <link rel="preconnect">
+				if (link.getAttribute("rel") == "preconnect") {
+					continue;
+				}
+				// Don't download <link rel="canonical">
+				// if (link.getAttribute("rel") == "canonical") {
+				// 	continue;
+				// }
+
+				const linkUrl = new URL(link.getAttribute("href"), url.toString());
+				if (link.tagName === 'a') {
+					page_links[linkUrl.toString()] = linkUrl;
+				} else {
+					dependencies[linkUrl.toString()] = linkUrl;
+				}
+			}
+
+			const imgs = root.findElements("//img[@src]");
+			for (const img of imgs) {
+				const linkUrl = new URL(img.getAttribute("src"), url.toString());
+				dependencies[linkUrl.toString()] = linkUrl;
+			}
+
+			const scripts = root.findElements("//script[@src]");
+			for (const script of scripts) {
+				const linkUrl = new URL(script.getAttribute("src"), url.toString());
+				dependencies[linkUrl.toString()] = linkUrl;
+			}
+		} else if (req.content_type.startsWith("text/css")) {
+			// Find url(...) on CSS
+			const matches = [...req.body.matchAll(GDBWarc.urlCssRegexp)];
+			for (const match of matches) {
+				const linkUrl = new URL(match[1], url.toString());
+				dependencies[linkUrl.toString()] = linkUrl;
+			}
+		} else if (req.content_type.startsWith("image/") || req.content_type.startsWith("font/")) {
+			// Nothing to do
+		} else if (req.content_type !== "") {
+			console.error('Unexpected content-type for '+url+':', req.content_type);
+		} else {
+			console.error('Unknown content-type for '+url+':', req);
+		}
+
+		for (const dependency_url in dependencies) {
+			const url = dependencies[dependency_url];
+			if (!this.#downloadedFiles[url.toString()] && this.should_add_file(url)) {
+				await this.#add_page(url, page_recursion, dep_recursion-1);
+			}
+		}
+
+		for (const link_url in page_links) {
+			const url = page_links[link_url];
+			if (!this.#downloadedFiles[url.toString()] && this.should_add_page(url)) {
+				await this.#add_page(url, page_recursion-1, dep_recursion);
+			}
+		}
 	}
 
 	async finish() {
@@ -162,58 +288,13 @@ async function main() {
 	const proxy_url = '127.0.0.1:'+port;
 	const gen = new GDBWarc('offline5.warc', proxy_url);
 	await gen.start();
-	await gen.addPage('/pt/');
+	await gen.addPage('/', 200, 15);
+	await gen.addPage('/en', 200, 15);
+	await gen.addPage('/pt', 200, 15);
+	await gen.addPage('/zh', 200, 15);
 	await gen.finish();
 }
 
 if (require.main === module) {
 	main()
-}
-
-
-
-
-function makeHttpRequest(url) {
-	return new Promise((resolve, reject) => {
-		var req = http.get(url,
-			// {
-			// 	lookup: (hostname, options, callback) => {
-			// 		console.log(hostname)
-			// 		console.log(options)
-			// 		console.log(callback)
-			// 		console.log(dns.lookup(hostname, options, (err, address, family) => {
-			// 			console.log(err);
-			// 			console.log('address: %j family: IPv%s', address, family);
-			// 		}))
-			// 		callback(null, '127.0.0.1', 4)
-			// 	}
-			// },
-			(res) => {
-			var response_headers = '';
-			for (var i=0; i < res.rawHeaders.length; i++) {
-				if (i%2 == 0) {
-					response_headers += res.rawHeaders[i]+': ';
-				} else {
-					response_headers += res.rawHeaders[i]+'\r\n';
-				}
-			}
-
-			let rawData = '';
-			res.on('data', (chunk) => { rawData += chunk; });
-			res.on('end', (chunk) => {
-				// console.log(rawData);
-				resolve({
-					'response_headers': res.headers,
-					'response_headers_raw': response_headers,
-					'request_headers_raw': res.req._header,
-					'data': rawData
-				})
-			});
-		});
-
-		req.on('error', (e) => {
-		  console.error(`problem with request: ${e.message}`);
-		  reject(e);
-		});
-	});
 }
