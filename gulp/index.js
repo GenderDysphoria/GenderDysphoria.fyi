ce1987e7639a69c79bc2001edd43f87bf8bbff73
G Queiroz <gabrieljvnq@gmail.com>
2022-06-01 17:30:59 -0300

    Removed offline task from defaults

--- gulp/index.js
+++ gulp/index.js
@@ -1,5 +1,5 @@
 
-const { series, watch, src, dest, parallel } = require('gulp');
+const { series, watch, src, dest } = require('gulp');
 const log = require('fancy-log');
 const chalk = require('chalk');
 
@@ -34,17 +34,15 @@ function copyProd () {
   return src('dist/**/*').pipe(dest('published'));
 }
 
-var TheServer = undefined;
-
 /** **************************************************************************************************************** **/
 
-exports.offline = offlineTask;
+exports.offline_quick = offlineTask;
+exports.offline_full = offline_full;
 exports.dev  = series(devBuildTask);
 exports.prod = series(prodBuildTask);
 exports.publish = series(
   cleanTask,
   prodBuildTask,
-  parallel(server, offlineTaskAutoStop),
   pushToProd,
   cleanTask.prodBackup,
   copyProd,
@@ -80,48 +78,37 @@ function watcher () {
   server();
 }
 
-async function offlineTaskSlow(callback) {
-  // Give some time for the server to start
-  const time = 2500;
-  await new Promise(resolve => setTimeout(resolve, time));
+// This thing is buggy as hell and barely works
+async function offline_full(callback) {
+  const { spawn } = require('child_process');
+  log(process.execPath);
+  const srv_proc = spawn(process.execPath, ['./server.js']);
+  srv_proc.stdout.on('data', (data) => {
+    process.stdout.write(data);
+  });
+  srv_proc.stderr.on('data', (data) => {
+    process.stderr.write(data);
+  });
+
+  await new Promise(resolve => setTimeout(resolve, 500));
+
+  // Run the main task
   offlineTask();
+
+  await new Promise(resolve => setTimeout(resolve, 500));
+
+  // Finish
+  srv_proc.kill('SIGTERM');
   if (callback !== undefined) {
     callback();
   }
 }
-
-async function offlineTaskAutoStop(callback) {
-  // replace this logic with manual spawning server in background process and killing it afterwards?
-
-  // problem with request: connect ECONNREFUSED 127.0.0.1:8085
-// (node:90782) UnhandledPromiseRejectionWarning: Error: connect ECONNREFUSED 127.0.0.1:8085
-//     at TCPConnectWrap.afterConnect [as oncomplete] (net.js:1159:16)
-//     at TCPConnectWrap.callbackTrampoline (internal/async_hooks.js:130:17)
-// (node:90782) UnhandledPromiseRejectionWarning: Unhandled promise rejection. This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). To terminate the node process on unhandled promise rejection, use the CLI flag `--unhandled-rejections=strict` (see https://nodejs.org/api/cli.html#cli_unhandled_rejections_mode). (rejection id: 1)
-// (node:90782) [DEP0018] DeprecationWarning: Unhandled promise rejections are deprecated. In the future, promise rejections that are not handled will terminate the Node.js process with a non-zero exit code.
-  var forever = require('forever');
-
-  // Give some time for the server to start
-  const time = 2500;
-  await new Promise(resolve => setTimeout(resolve, time));
-  offlineTask();
-
-  if (TheServer !== undefined) {    
-    log(TheServer.stop());
-  }
-
-  if (callback !== undefined) {
-    callback();
-  }
-}
-
-exports.offline2 = parallel(server, offlineTaskAutoStop);
 
 function server (callback) {
   var forever = require('forever');
-  TheServer = new forever.Monitor('server.js', {fork: true});
-  TheServer.start();
-  forever.startServer(TheServer);
+  var srv = new forever.Monitor('server.js', {fork: true});
+  srv.start();
+  forever.startServer(srv);
 
   if (callback !== undefined) {
     callback();
@@ -129,7 +116,7 @@ function server (callback) {
 }
 
 exports.watch = series(devBuildTask, watcher);
-exports.uat = series(cleanTask, prodBuildTask, parallel(server, offlineTaskSlow));
+exports.uat = series(cleanTask, prodBuildTask, server);
 
 /** **************************************************************************************************************** **/
 
