665f1ff991488b20ead2682dab9074573469c633
G. Queiroz <gabrieljvnq@gmail.com>
2022-06-10 23:54:51 -0300

    Code cleanup

--- build/glossary.js
+++ build/glossary.js
@@ -104,7 +104,7 @@ class Entry {
 	_show = false;
 	_show_in_print = false;
 	_words = new Map();
-	
+
 	constructor(key, src) {
 		if (isNonEmptyString(key)) {
 			this._key = key;
@@ -214,23 +214,29 @@ class Glossary {
 		return [entry_obj, word_obj];
 	}
 
-	render_html_word(word_str) {
+	// The `full` parameter should always be true except for when glossing tooltips themselves. This is to prevent inifnite recursion.
+	render_block(word_str, full) {
 		const [entry_obj, word_obj] = this.getEntryAndWordObj(word_str);
 		const has_description = entry_obj.description.length > 0;
 		const has_ruby        = word_obj.ruby != undefined;
 		const short_desc      = entry_obj.description[0];
-		
+
 		if (has_description === false && has_ruby === false) {
 			return word_str;
 		}
 
 		const word_rendered = word_obj.renderAs || word_str;
-		
+
+		let main_link = '#';
+		if (full === false && this.glossary_url != undefined) {
+			main_link = `${this.glossary_url}#${entry_obj.key}`;
+		}
+
 		let word_core = '';
 		if (has_description) {
-			word_core += '<dfn class="glossed-main"><a href="#">'+word_rendered+'</a></dfn>';
+			word_core = `<dfn class="glossed-main"><a href="${main_link}">${word_rendered}</a></dfn>`;
 		} else {
-			word_core += word_rendered;
+			word_core = `<a href="${main_link}">${word_rendered}</a>`;
 		}
 
 		let core = '';
@@ -249,26 +255,43 @@ class Glossary {
 			core += word_core;
 		}
 
-		if (!has_description) {
+		if (!has_description || full === false) {
 			return core;
 		}
 
 		let print = '';
 		if (entry_obj.show_in_print) {
-			print += `<span class="glossed-print">${short_desc}</span>`;
+			print += `<span class="glossed-print">(${short_desc}) </span>`;
 		}
 
 		let tooltip = '';
 		let read_more = '';
 		if (this.glossary_url != undefined) {
 			const entry_link = `${this.glossary_url}#${entry_obj.key}`;
-			log(entry_link)
 			read_more += ` <a href="${entry_link}">${i18n(this.lang, 'GLOSSARY_READ_MORE')}</a>`;
 		}
 		tooltip += `<span class="glossed-tooltip">${short_desc}${read_more}</span>`;
-	
+
 		const final = `<span class="glossed-block">${core}${print}${tooltip}</span>`;
 		return final;
+	}
+
+	auto_gloss_html(src_html) {
+		// Steps
+		// 1. Parse HTML
+		// 2. Traverse the text nodes
+		// 3. Break words
+		// 4. Replace the necessary ones with the full block
+
+		// For chinese:
+		// Run regexps to replace the char-seqs with themselves but sourunded by U+0091 characters to act as word breaks. Then continue with step 3
+
+		// Word breaking regexp: /(\b|\u0091)/g
+	}
+
+	render_full_glossary() {
+		// make the whole <dl><dt><dd> thing
+		// auto gloss the words in the entries descriptions
 	}
 }
 
@@ -282,7 +305,7 @@ class MultiGlossary {
 
 	static async load_default() {
 		const ans = new MultiGlossary();
-		const filepaths = await glob('public/**/_glossary.js', { cwd: ROOT, nodir: true });	
+		const filepaths = await glob('public/**/_glossary.js', { cwd: ROOT, nodir: true });
 		for (const filepath of filepaths) {
 			try {
 				const gloss_src = require(path.join('..', filepath));
@@ -297,7 +320,7 @@ class MultiGlossary {
 	}
 
 	get glossaries() { return this._glossaries; }
-	
+
 	by_lang (lang) { return this._glossaries.get(lang); }
 
 }
@@ -307,109 +330,10 @@ async function main() {
 	log(glossaries);
 	const en_gloss = glossaries.by_lang('en');
 	log(en_gloss);
-	log(en_gloss.render_html_word('AMAB'));
+	log(en_gloss.render_block('AMAB', false));
+	log(en_gloss.render_block('AMAB', true));
 }
 
-// TODO: change structure for the module to hold everything
-// TODO: change structure to separate meaning from words
-/***
- * {
- *   'words': {
- *		'transman': {
- * 			'meaning': 'transman',
- * 			'class': 'noun',
- * 			'relation': '', // main form uses an empty string
- * 			'show': true, // shows in the glossary entry for the main form
- * 			'own_entry': true,
- * 			'auto_gloss': true,
- * 		},
- * 		'transmen': {
- * 			'meaning': 'transman',
- * 			'class': 'noun',
- * 			'relation': 'plural',
- * 			'show': true,
- * 			'own_entry': false,
- * 			'auto_gloss': true,
- * 		}, ...
- * 		'GLAAD': {
- * 			'ruby': '/ɡlæd/',
- * 			'auto_gloss': true,
- * 		},
- *   },
- * 	'meanings': {
- * 		'transman': {
- * 			'title': 'trans·man',
- * 			'short': 'An AFAB person who identifies as a man.',
- * 			'long': 'A person who was "born as a woman" but "became a man".'
- * 		}, ...
- * 	}
- * }
- */
-
-async function loadGlossaries() {
-	log('loading glossaries');
-	const filepaths = await glob('public/**/_glossary.js', { cwd: ROOT, nodir: true });
-
-	const output = {};
-	for (const filepath of filepaths) {
-		// Load public/**/_glossary.js files
-		const src_gloss = require(path.join('..', filepath));
-		const lang = path.basename(path.dirname(filepath));
-
-		const terms = [];
-		const terms_map = new Map();
-		const entries = Object.keys(src_gloss.entries).sort();
-
-		// Process each entry
-		for (const entry_name of entries) {
-			// Sanity check
-			if (terms_map.has(entry_name)) {
-				throw Error('conflict of definitions for term: '+entry_name);
-			}
-
-			// Normalize the entry
-			const src_entry = src_gloss.entries[entry_name];
-			const entry = {
-				main_form: entry_name,
-				ruby: src_entry.ruby || undefined,
-				short: src_entry.short || undefined,
-				long: src_entry.long || undefined,
-				pronunciations: src_entry.pronunciations || [],
-				variants: src_entry.variants || [],
-				renderAs: src_entry.renderAs || {},
-				pronunciations_to_include_in_short_form: src_entry.pronunciations_to_include_in_short_form || 0,
-			};
-			// Add the entry itself to our map and the enrty name to our lists
-			terms_map.set(entry_name, entry);
-			terms.push(entry_name);
-
-			// Process the term variants
-			for (const variant_name of entry.variants) {
-				// Sanity check
-				if (terms_map.has(variant_name)) {
-					throw Error('conflict of definitions for term: '+variant_name);
-				}
-
-				// Add the entry itself to our map and the variant name to our list
-				terms_map.set(variant_name, entry);
-				terms.push(variant_name);
-			}
-		}
-
-		// Finalize stuff
-		const out_gloss = {
-			'glossary_url': src_gloss.glossary_url,
-			'lang': src_gloss.lang,
-			'terms': terms, // sorted list of terms and variants
-			'entries': entries, // sorted list of terms
-			'map': terms_map, // map of terms and variants to definitions
-			'set': new Set(terms) // set of terms and variants
-		};
-		output[lang] = out_gloss;
-	}
-	return output;
-}
-module.exports.loadGlossaries = loadGlossaries;
 
 function autoInsertGloss(input, glossary) {
 	// Split at word boundaries
@@ -437,106 +361,13 @@ function autoInsertGloss(input, glossary) {
 	// Concatenate out words back into a simple string
 	return words.join('');
 }
-module.exports.autoInsertGloss = autoInsertGloss;
-
-const punctuation_regexp = /[.,:;!@]/;
-
-function isFirstPunctuation(span) {
-	if (span === undefined) {
-		return undefined;
-	}
-	return punctuation_regexp.test(span.charAt(0));
-}
-
-function makeHTMLGloss(term_key, glossary, next_word) {
-	const entry = glossary.map.get(term_key);
-	const term = entry.renderAs[term_key] || term_key;
-	const has_glossary_definition = entry.short !== undefined || entry.long !== undefined;
-	const term_core = has_glossary_definition ?
-		`<dfn class="glossed-main"><a href="_">`+term+`</a></dfn>` : term;
-	const gloss_url = glossary.glossary_url;
-	const lang = glossary.lang;
-	const read_more_txt = i18n(lang, 'GLOSSARY_READ_MORE');
-	const go_to_txt = i18n(lang, 'GLOSSARY_GO_TO_GLOSSARY');
-
-	var output = ``;
-	if (has_glossary_definition) {
-		output += `<span class="glossed-block">`;
-	}
-
-	// Make ruby (asiatic pronunciation annotation) markup
-	if (entry.ruby !== undefined) {
-		if (has_glossary_definition) {
-			output += `<ruby class="glossed-ruby">`;
-		} else {
-			output += `<ruby>`;
-		}
-		output += term_core;
-		output += `<rp>(</rp><rt>`;
-		output += entry.ruby;
-		output += `</rt><rp>)</rp>`
-		output += `</ruby>`
-	} else {
-		output += term_core;
-	}
-
-	// Aggregate pronunciations
-	var i = 0;
-	var pronunciations = ``;
-	if (entry.pronunciations_to_include_in_short_form > 0) {
-		for (const pronunciation of entry.pronunciations) {
-			if (i <= entry.pronunciations_to_include_in_short_form) {
-				break;
-			}
-
-			if (pronunciation.IPA !== undefined) {
-				if (pronunciations.length !== 0) {
-					pronunciations += ', ';
-				}
-				pronunciations += pronunciation.IPA;
-				i++;
-			}
-		}
-		if (pronunciations.length !== 0) {
-			pronunciations += `. `;
-			pronunciations = `<span class="pronunciations">`+pronunciations+`</span>`;
-		}
-	}
-
-	// Finalize short form
-	const short_form = pronunciations+(entry.short||'');
-
-	// Make tooltip print
-	if (entry.short !== undefined && entry.show_in_print !== false) {
-		assert(has_glossary_definition == true);
-		output += `<span class="glossed-print">`;
-		output += ` (`+short_form+`)`;
-		if (isFirstPunctuation(next_word) === false) {
-			output += ` `;
-		}
-		output += `</span>`;
-	}
-
-	// Make tooltip markup
-	if (has_glossary_definition) {
-		output += `<span class="glossed-tooltip">`;
-		if (gloss_url !== undefined) {
-			const entry_url = `${gloss_url}/_entry_${entry.main_form}`;
-			if (short_form !== undefined) {
-				output += short_form+` <a href="${entry_url}">${read_more_txt}</a>`;
-			} else {
-				output += `<a href="${entry_url}">${go_to_txt}</a>`;
-			}
-		}
-		output += `</span>`;
-	}
-
-	if (has_glossary_definition) {
-		output += `</span>`;
-	}
-	return output;
-}
 
 if (require.main === module) {
 	main()
+}
+
+module.exports = {
+	Glossary: Glossary,
+	Entry: Entry,
+	Word: Word
 }
