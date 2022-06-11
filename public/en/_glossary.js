868820fb9289c62a709ddf17f27a8bcbc61a96f5
G. Queiroz <gabrieljvnq@gmail.com>
2022-06-10 22:43:02 -0300

    New glossary format

--- public/en/_glossary.js
+++ public/en/_glossary.js
@@ -1,81 +1,84 @@
 module.exports = exports = {
-	'glossary_url': '/en/glossary',
-	'lang': 'en',
-	'entries': {
+	glossary_url: '/en/glossary',
+	lang: 'en',
+	word_break_by_regex: true, // only useful for Chinese
+	entries: {
 		'transgender': {
-			'title': 'trans·gen·der',
-			'short': 'Denoting or relating to a person whose sense of personal identity and gender does not correspond with their sex assigned at birth.',
-			'class': 'adjective',
-			'variants': ['transgenders'],
-			'related': {
+			title: 'trans·gen·der',
+			description: ['Denoting or relating to a person whose sense of personal identity and gender does not correspond with their sex assigned at birth.'],
+			show: true,
+			words: {
+				'transgender': {
+					class: 'adjective',
+					auto_gloss: true,
+					show: true,
+					relation: '='
+				},
 				'trans': {
-					'form': 'short',
+					class: 'adjective',
+					auto_gloss: true,
 					show: true,
-				},
-				'trans*': {
-					'form': 'unusual spelling',
+					relation: 'abbreviation'
+				}
+			}
+		},
+		'amab': {
+			title: 'AMAB',
+			description: ['Assinged Male At Birth.'],
+			show: true,
+			words: {
+				'AMAB': {
+					class: 'noun',
+					auto_gloss: true,
 					show: true,
+					relation: '=',
+					pronunciations: {
+						common: 'EY-MABB',
+					}
 				},
-			},
-		},
-		'AMAB': {
-			'pronunciations': [
-				{
-					'common': 'EY-MABB',
-					'IPA': undefined,
-					'aria': undefined
+				'AMABs': {
+					class: 'noun',
+					auto_gloss: true,
+					show: false,
+					relation: 'plural'
 				}
-			],
-			'short': 'Assinged Male At Birth',
-			'long': 'Usually AMABs have anatomically male bodies at birth but intersex people are sometimes AMAB.',
-			'variants': ['AMABs'],
+			}
 		},
-		'AFAB': {
-			'pronunciations': [
-				{
-					'common': 'EY-FABB',
-					'IPA': undefined,
-					'aria': undefined
+		'afab': {
+			title: 'AFAB',
+			description: ['Assinged Female At Birth.'],
+			show: true,
+			words: {
+				'AFAB': {
+					class: 'noun',
+					auto_gloss: true,
+					show: true,
+					relation: '=',
+					pronunciations: {
+						common: 'EY-FABB',
+					}
+				},
+				'AFABs': {
+					class: 'noun',
+					auto_gloss: true,
+					show: false,
+					relation: 'plural'
 				}
-			],
-			'short': 'Assigned Female At Birth',
-			'long': 'Usually AFABs have anatomically female bodies at birth but some intersex babies are AFAB either because of how their genitalia looked at birth or because the doctors found it easier to convert the baby\'s ambiguous genitalia into a vagina.\n'+
-				'In extreme cases, babies whose circumcision was botched are sort of AFAB as their male status is quickly erased from records or was never oficialized as the birth certificate is sometimes done after any circumcision.',
-			'variants': ['AFABs'],
+			}
 		},
 		'GLAAD': {
-			'ruby': '/ɡlæd/',
-			'pronunciations': [
-				{
-					'IPA': '/ɡlæd/',
-				},
-			],
-			'short': 'Gay & Lesbian Alliance Against Defamation',
-			'long': 'An americna organization that protests defamatory coverage of queer people and pushes for their acceptance. Oficial website: https://www.glaad.org/',
-		},
-		'TeX': {
-			'renderAs': {
-				'TeX': '<span style="font-family: \'CMU Serif\', cmr10, LMRoman10-Regular, \'Latin Modern Math\', \'Nimbus Roman No9 L\', \'Times New Roman\', Times, serif;">T<span style="text-transform: uppercase; vertical-align: -0.25em; margin-left: -0.1667em; margin-right: -0.125em; line-height: 1ex;">e</span>X</span>',
-			},
-		},
-		'LaTeX': {
-			'ruby': '/ˈlɑːtɛx/',
-			'pronunciations': [
-				{
-					'common': 'LAH-tekh',
-					'IPA': '/ˈlɑːtɛx/',
-					'aria': undefined
-				},
-				{
-					'common': 'LAY-tekh',
-					'IPA': '/ˈleɪtɛx/',
-					'aria': undefined
-				},
-			],
-			'short': 'A document markup language used in academia. Styled as LaTeX or LaTeX2e',
-			'long': 'A set of macros over the TeX typesetting designed to aid in the <em>preparation</em> of *professional* documents.',
-			'renderAs': {
-				'LaTeX': '<span style="font-family: \'CMU Serif\', cmr10, LMRoman10-Regular, \'Latin Modern Math\', \'Nimbus Roman No9 L\', \'Times New Roman\', Times, serif;">L<span style="text-transform: uppercase; font-size: 0.75em; vertical-align: 0.25em; margin-left: -0.36em; margin-right: -0.15em; line-height: 1ex;">a</span>T<span style="text-transform: uppercase; vertical-align: -0.25em; margin-left: -0.1667em; margin-right: -0.125em; line-height: 1ex;">e</span>X</span>',
+			title: 'GLAAD',
+			description: ['An americna organization that protests defamatory coverage of queer people and pushes for their acceptance.', ' Oficial website: https://www.glaad.org/'],
+			words: {
+				'GLADD': {
+					class: 'noun',
+					auto_gloss: true,
+					show: true,
+					relation: '=',
+					pronunciations: {
+						ipa: '/ɡlæd/',
+					}
+				}
 			}
 		},
 	},
