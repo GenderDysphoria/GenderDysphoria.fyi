8676ff0b0c61cb4c7cba179392bb3cb0a30d5e4f
Maxime van der Smissen <m.vandersmissen@student.avans.nl>
2023-07-13 19:11:50 +0200

    Add Dutch nonpage language files and index page
    Added files to public/nl/ dir:
      _concat.json
      _disclaimer.hbs
      _menu.hbs
      _strings.js
      index.md

---
+++ public/nl/_strings.js
@@ -0,0 +1,18 @@
+
+module.exports = exports = {
+  MENU_LANGUAGE: 'Nederlands (NL)',
+  SITE_TITLE: 'Dat is Genderdysforie, FYI',
+  HEADER_TITLE: 'Dat is Genderdysforie, <abbr title="Ter Informatie (EN: For Your Information)">FYI</abbr>',
+  SITE_DESCRIPTION: 'Een bron voor degenen die hun gender in twijfel trekken, al op een genderreis zijn of gewoon meer willen weten over wat het betekent om transgender te zijn.',
+
+  PATREON_FOOTER_BODY: 'Deze site is mogelijk gemaakt dankzij bijdragen vanuit de gemeenschap. Als je deze site waardevol hebt gevonden, overweeg dan <a href="https://www.patreon.com/curvyandtrans">een toezegging op Patreon te doen</a> of een <a href="https://ko-fi.com/curvyandtrans">donatie via Ko-fi</a>.',
+  PATREON_FOOTER_THANKS: 'Speciale dank aan de volgende Patreon-ondersteuners:',
+  FOOTER_COPYRIGHT: 'Tekst van de site, websitecode en originele afbeeldingen zijn <a href="https://creativecommons.org/licenses/by-nc-sa/2.0/">gelicentieerd onder CC BY-NC-SA.</a>',
+  FOOTER_COPYRIGHT2: 'en Andere Bijdragers',
+
+  TRANS_TWITTER_TOPICS: 'Trans Twitter Onderwerpen',
+  MISSING_TWEET_FOR: 'Ontbrekende tweet voor ',
+  QUOTED_TWEET_UNAVAILABLE: 'Geciteerde tweet niet beschikbaar',
+  TWEET_DATE_FORMAT: 'HH:mm - dd LLL. yyyy',
+};
+
