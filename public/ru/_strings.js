f12ba792f46578e3a70a998235dbf790c57ef43a
Fancryer <vadimir.kots@mail.ru>
2024-10-04 19:22:41 +0400

    Copleted Russian translation

---
+++ public/ru/_strings.js
@@ -0,0 +1,16 @@
+
+module.exports = exports = {
+  SITE_TITLE: 'Это гендерная дисфория, FYI',
+  SITE_DESCRIPTION: 'Ресурс для тех, кто сомневается в своем гендере, уже находится в гендерном путешествии или просто хочет узнать больше о том, что значит быть трансгендером.',
+
+  HEADER_TITLE: 'Это гендерная дисфория, <abbr title="К вашему сведению">FYI</abbr>',
+  PATREON_FOOTER_BODY: 'Этот сайт стал возможен благодаря вкладу сообщества. Если вы считаете этот сайт бесценным, рассмотрите возможность <a href="https://www.patreon.com/curvyandtrans">внесения залога на Patreon@</a> или <a href="https://ko-fi.com/curvyandtrans">пожертвования через Ko-fi<a/>.',
+  PATREON_FOOTER_THANKS: 'Особая благодарность следующим патронам:',
+  FOOTER_COPYRIGHT: 'Текст на сайте, его исходный код, а также оригинальные изображения <a href="https://creativecommons.org/licenses/by-nc-sa/2.0/">находятся под лицензией CC BY-NC-SA.</a>',
+  FOOTER_COPYRIGHT2: 'и другие контрибьюторы',
+
+  'TRANS_TWITTER_TOPICS': 'Трансгендерные темы на Twitter',
+
+  MENU_LANGUAGE: 'Русский (RU)',
+};
+
