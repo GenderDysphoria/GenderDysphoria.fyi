60f1d74d7c0de2dbf6251bafeb02ebeb846de982
Pichu <pichu.baby@gmail.com>
2024-11-15 02:44:32 +0800

    Update Taiwanese Chinese translations and improve clarity in the Gender Dysphoria content

--- public/tw/_strings.js
+++ public/tw/_strings.js
@@ -1,18 +1,18 @@
 
 module.exports = exports = {
-  MENU_LANGUAGE: '台式中文 (TW)',
-  SITE_TITLE: 'That\'s Gender Dysphoria, FYI',
-  HEADER_TITLE: 'That\'s Gender Dysphoria, <abbr title="For Your Information">FYI</abbr>',
-  SITE_DESCRIPTION: 'A resource for those questioning their gender, already on a gender journey, or simply wanting to learn more about what it is to be transgender.',
+  MENU_LANGUAGE: '台灣華語 (TW)',
+  SITE_TITLE: '這就是性別不安，FYI',
+  HEADER_TITLE: '這就是性別不安，<abbr title="For Your Information">FYI</abbr>',
+  SITE_DESCRIPTION: '為那些正在質疑自己性別、已在性別探索旅程中，或只是想了解更多關於跨性別者資訊的人提供的資源。',
 
-  PATREON_FOOTER_BODY: 'This site is made possible thanks to contributions from the community. If you have found this site invaluable, please consider <a href="https://www.patreon.com/curvyandtrans">making a pledge on patreon</a> or a <a href="https://ko-fi.com/curvyandtrans">donation via Ko-fi</a>.',
-  PATREON_FOOTER_THANKS: 'Special thanks to the following patreon supporters:',
-  FOOTER_COPYRIGHT: 'Site text, website code and original graphics are <a href="https://creativecommons.org/licenses/by-nc-sa/2.0/">licensed CC BY-NC-SA.</a>',
-  FOOTER_COPYRIGHT2: 'and Other Contributors',
+  PATREON_FOOTER_BODY: '感謝社群的貢獻，才得以讓本網站順利運作。如果您覺得本網站很有價值，請考慮<a href="https://www.patreon.com/curvyandtrans">在 Patreon 上贊助</a>或<a href="https://ko-fi.com/curvyandtrans">透過 Ko-fi 捐款</a>。',
+  PATREON_FOOTER_THANKS: '特別感謝以下 Patreon 支持者：',
+  FOOTER_COPYRIGHT: '網站文字、網站程式碼和原創圖像 <a href="https://creativecommons.org/licenses/by-nc-sa/2.0/">採用 CC BY-NC-SA 授權。</a>',
+  FOOTER_COPYRIGHT2: '以及其他貢獻者',
 
-  TRANS_TWITTER_TOPICS: 'Trans Twitter Topics',
-  MISSING_TWEET_FOR: 'Missing tweet for ',
-  QUOTED_TWEET_UNAVAILABLE: 'Quoted Tweet Unavailable',
+  TRANS_TWITTER_TOPICS: '跨性別推特話題',
+  MISSING_TWEET_FOR: '缺少推文：',
+  QUOTED_TWEET_UNAVAILABLE: '引用的推文無法使用',
   TWEET_DATE_FORMAT: 'h:mm aa - EEE, LLL do, yyyy',
 };
 
