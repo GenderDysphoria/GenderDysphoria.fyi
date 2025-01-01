d251ebd6d81edaa26f14f1caa1a15a6bf2e8ec54
evasquare <177620651+evasquare@users.noreply.github.com>
2025-01-01 18:53:28 +0900

    Translate _strings.js

--- public/ko/_strings.js
+++ public/ko/_strings.js
@@ -1,16 +1,15 @@
 
 module.exports = exports = {
-  SITE_TITLE: 'That\'s Gender Dysphoria, FYI',
-  SITE_DESCRIPTION: 'A resource for those questioning their gender, already on a gender journey, or simply wanting to learn more about what it is to be transgender.',
+  SITE_TITLE: '그게 젠더 불쾌감입니다, FYI',
+  SITE_DESCRIPTION: '자신의 젠더를 질문 중인 사람들, 젠더 여정을 이미 시작한 사람들, 그리고 단순히 트랜스젠더가 되는 것이 어떤 것인지 배워보고 싶은 사람들을 위한 자료.',
 
-  HEADER_TITLE: 'That\'s Gender Dysphoria, <abbr title="For Your Information">FYI</abbr>',
-  PATREON_FOOTER_BODY: 'This site is made possible thanks to contributions from the community. If you have found this site invaluable, please consider <a href="https://www.patreon.com/curvyandtrans">making a pledge on patreon</a> or a <a href="https://ko-fi.com/curvyandtrans">donation via Ko-fi</a>.',
-  PATREON_FOOTER_THANKS: 'Special thanks to the following patreon supporters:',
-  FOOTER_COPYRIGHT: 'Site text, website code and original graphics are <a href="https://creativecommons.org/licenses/by-nc-sa/2.0/">licensed CC BY-NC-SA.</a>',
-  FOOTER_COPYRIGHT2: 'and Other Contributors',
+  HEADER_TITLE: '그게 젠더 불쾌감입니다, <abbr title="For Your Information">FYI</abbr>',
+  PATREON_FOOTER_BODY: '이 웹사이트는 공동체의 기여 덕분에 만들어질수 있었습니다. 이 웹사이트가 귀중한 가치가 되었다면, <a href="https://www.patreon.com/curvyandtrans">Patreon에서 후원</a>하거나 <a href="https://ko-fi.com/curvyandtrans">Ko-fi를 통해 기부</a>하는 것을 고려해주세요.',
+  PATREON_FOOTER_THANKS: '다음 Patreon 서포터들에게 깊은 감사를 드립니다:',
+  FOOTER_COPYRIGHT: '사이트 텍스트와 웹사이트 코드, 원본 그래픽들은 <a href="https://creativecommons.org/licenses/by-nc-sa/2.0/">CC BY-NC-SA 라이선스를 사용합니다.</a>',
+  FOOTER_COPYRIGHT2: '그리고 다른 기여자들',
 
-  'TRANS_TWITTER_TOPICS': 'Trans Twitter Topics',
+  'TRANS_TWITTER_TOPICS': '트랜스젠더 트위터 주제',
 
-  MENU_LANGUAGE: 'English (EN)',
+  MENU_LANGUAGE: '한국어 (KO)',
 };
-
