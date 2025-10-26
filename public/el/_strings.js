d680abb27d6d28513ce020e90aa87220cf453461
Ourania J. Banti <68644945+ouranya@users.noreply.github.com>
2025-10-26 21:04:53 +0100

    Update aux strings (el)

--- public/el/_strings.js
+++ public/el/_strings.js
@@ -8,7 +8,7 @@ module.exports = exports = {
   PATREON_FOOTER_BODY: 'Αυτή η ιστοσελίδα είναι διαθέσιμη χάρις σε κοινοτικές συνεισφορές. Εάν η ιστοσελίδα σάς φάνηκε επωφελής, εξετάστε το ενδεχόμενο να κάνετε μια δωρεά <a href="https://www.patreon.com/curvyandtrans">στο patreon</a> ή <a href="https://ko-fi.com/curvyandtrans">στο Ko-fi</a>.',
   PATREON_FOOTER_THANKS: 'Ιδιαίτερες ευχαριστίες στους παρακάτω υποστηρικτές από το patreon:',
   FOOTER_COPYRIGHT: 'Το κείμενο, οι εικόνες, και ο κώδικας της ιστοσελίδας είναι <a href="https://creativecommons.org/licenses/by-nc-sa/2.0/">αδειοδοτημένες κατά CC BY-NC-SA.</a>',
-  FOOTER_COPYRIGHT2: 'και σε άλλα άτομα που έχουν συνεισφέρει',
+  FOOTER_COPYRIGHT2: 'και άλλα άτομα που έχουν συνεισφέρει',
 
   TRANS_TWITTER_TOPICS: 'Θέματα Trans Twitter',
   MISSING_TWEET_FOR: 'Έλλειψη tweet για ',
