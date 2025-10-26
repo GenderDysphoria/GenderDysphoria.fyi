0f0bc10cdf3530d13980aa011402e7e35aee7433
Ourania J. Banti <68644945+ouranya@users.noreply.github.com>
2025-10-26 15:11:33 +0100

    Translate auxiliary strings to Greek

---
+++ public/el/_strings.js
@@ -0,0 +1,18 @@
+
+module.exports = exports = {
+  MENU_LANGUAGE: 'Ελληνικά (EL)',
+  SITE_TITLE: 'Αυτό λέγεται Δυσφορία Φύλου, FYI',
+  HEADER_TITLE: 'Αυτό λέγεται Δυσφορία Φύλου, <abbr title="Προς ενημέρωσή σας">FYI</abbr>',
+  SITE_DESCRIPTION: 'Μια εγκυκλοπαίδεια για άτομα που αμφισβητούν το φύλο τους, βρίσκονται ήδη σε φυλομετάβαση, ή απλώς θέλουν να μάθουν περισσότερα σχετικά με το τι σημαίνει να είσαι τρανς.',
+
+  PATREON_FOOTER_BODY: 'Αυτή η ιστοσελίδα είναι διαθέσιμη χάρις σε κοινοτικές συνεισφορές. Εάν η ιστοσελίδα σάς φάνηκε επωφελής, εξετάστε το ενδεχόμενο να κάνετε μια δωρεά <a href="https://www.patreon.com/curvyandtrans">στο patreon</a> ή <a href="https://ko-fi.com/curvyandtrans">στο Ko-fi</a>.',
+  PATREON_FOOTER_THANKS: 'Ιδιαίτερες ευχαριστίες στους παρακάτω υποστηρικτές από το patreon:',
+  FOOTER_COPYRIGHT: 'Το κείμενο, οι εικόνες, και ο κώδικας της ιστοσελίδας είναι <a href="https://creativecommons.org/licenses/by-nc-sa/2.0/">αδειοδοτημένες κατά CC BY-NC-SA.</a>',
+  FOOTER_COPYRIGHT2: 'και σε άλλα άτομα που έχουν συνεισφέρει',
+
+  TRANS_TWITTER_TOPICS: 'Θέματα Trans Twitter',
+  MISSING_TWEET_FOR: 'Έλλειψη tweet για ',
+  QUOTED_TWEET_UNAVAILABLE: 'Μη διαθέσιμο αναφερόμενο tweet',
+  TWEET_DATE_FORMAT: 'hh:mm - dd/MM/yyyy',
+};
+
