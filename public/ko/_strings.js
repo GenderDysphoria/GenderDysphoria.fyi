d2b4a1f5cb76ba3324f7d0d63fb0dade46fb8369
evasquare <177620651+evasquare@users.noreply.github.com>
2026-02-01 04:42:20 +0900

    Update _concat.json and _string.js

--- public/ko/_strings.js
+++ public/ko/_strings.js
@@ -2,12 +2,12 @@ module.exports = exports = {
   MENU_LANGUAGE: '한국어 (KO)',
   SITE_TITLE: '그게 젠더 불쾌감입니다, FYI',
   HEADER_TITLE: '그게 젠더 불쾌감입니다, <abbr title="For Your Information">FYI</abbr>',
-  SITE_DESCRIPTION: '자신의 젠더를 질문 중인 사람들, 젠더 여정을 이미 시작한 사람들, 그리고 단순히 트랜스젠더가 되는 것이 어떤 것인지 배워보고 싶은 사람들을 위한 자료.',
+  SITE_DESCRIPTION: '자신의 젠더에 의문을 가지는 사람들, 젠더 여정을 이미 시작한 사람들, 그리고 단순히 트랜스젠더인 것이 어떤 것인지 배워보고 싶은 사람들을 위한 자료.',
 
   PATREON_FOOTER_BODY: '이 웹사이트는 공동체의 기여 덕분에 만들어질 수 있었습니다. 이 웹사이트가 귀중한 가치가 되었다면, <a href="https://www.patreon.com/curvyandtrans">Patreon에서 후원</a>하거나 <a href="https://ko-fi.com/curvyandtrans">Ko-fi를 통해 기부</a>하는 것을 고려해 보세요.',
   PATREON_FOOTER_THANKS: '아래 Patreon 후원자들에게 깊은 감사를 드립니다:',
   FOOTER_COPYRIGHT: '사이트 텍스트와 웹사이트 코드, 원본 그래픽들은 <a href="https://creativecommons.org/licenses/by-nc-sa/2.0/">CC BY-NC-SA 라이선스를 사용합니다.</a>',
-  FOOTER_COPYRIGHT2: '와 그 외 다른 기여자들',
+  FOOTER_COPYRIGHT2: '와 다른 기여자들',
 
   TRANS_TWITTER_TOPICS: '트랜스젠더 트위터 주제',
   MISSING_TWEET_FOR: '다음 트윗을 찾을수 없음: ',
