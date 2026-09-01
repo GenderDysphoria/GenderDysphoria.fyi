0e5434719a85ef8da2c40537b44eb567e5035567
Not Coconut <mariembenmahmoud715@gmail.com>
2026-09-01 20:36:14 +0100

    Added Arabic Translation

---
+++ public/ar/_strings.js
@@ -0,0 +1,18 @@
+
+module.exports = exports = {
+  MENU_LANGUAGE: 'العربية (Ar)',
+  SITE_TITLE: 'هذا يُعرف بـ "اضطراب الهوية الاجتماعية"، للعلم.',
+  HEADER_TITLE: 'هذا يُعرف بـ "اضطراب الهوية الاجتماعية", <abbr title="For Your Information">للعلم</abbr>',
+  SITE_DESCRIPTION: 'مصدرٌ مخصصٌ لأولئك الذين يتساءلون عن هويتهم الج أو ممن هم بالفعل في رحلة استكشاف هويتهم الاجتماعية أو ببساطة لمن يرغبون في معرفة المزيد عما يعنيه أن يكون المرء عابراً جندريا.',
+
+  PATREON_FOOTER_BODY: 'يعتمد استمرار هذا الموقع على مساهمات المجتمع. إذا وجدت هذا الموقع قيّماً ومفيداً، يرجى التفكير في <a href="https://www.patreon.com/curvyandtrans">تقديم دعم عبر patreon</a> او عبر <a href="https://ko-fi.com/curvyandtrans">التبرع من خلال Ko-fi</a>.',
+  PATREON_FOOTER_THANKS: 'شكر خاص لداعمي حشفقثخى التاليين::',
+  FOOTER_COPYRIGHT: 'نص الموقع، ورمز الموقع الإلكتروني، والرسومات الأصلية هي <a href="https://creativecommons.org/licenses/by-nc-sa/2.0/">مرخّص بموجب رخصة CC BY-NC-SA.</a>',
+  FOOTER_COPYRIGHT2: 'وغيرهم من المساهمين',
+
+  TRANS_TWITTER_TOPICS: 'مواضيع تويتر المتعلقة بالمتحولين جنسياً',
+  MISSING_TWEET_FOR: 'تغريدة مفقودة لـ ',
+  QUOTED_TWEET_UNAVAILABLE: 'التغريدة المُقتبسة غير متاحة',
+  TWEET_DATE_FORMAT: 'h:mm aa - EEE, LLL do, yyyy',
+};
+
